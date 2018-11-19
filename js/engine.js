2222710d63ad0bd1d0f79b67760a26535079a6a1
Mandy Rose <mandycrose@gmail.com>
2018-11-19 14:33:53 +0000

    listen for button clicks

--- js/engine.js
+++ js/engine.js
@@ -1,4 +1,18 @@
 /*listen for the button clicks*/
-$("rock").click(function(event){
+$("#rock").click(function(event){
+var move_you = "rock";
+$("#play_you").text (move_you);
+console.log("you: " + move_you);
+});
 
+$("#paper").click(function(event){
+var move_you = "paper";
+$("#play_you").text (move_you);
+console.log("you: " + move_you);
+});
+
+$("#Scissors").click(function(event){
+var move_you = "Scissors";
+$("#play_you").text (move_you);
+console.log("you: " + move_you);
 });
