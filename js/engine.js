63804ef427479e729021ae0ce927708d46ee6220
mandycrose <mandycrose@gmail.com>
2018-11-26 11:50:18 +0000

    first git

--- js/engine.js
+++ js/engine.js
@@ -1,18 +1,57 @@
-/*listen for the button clicks*/
-$("#rock").click(function(event){
-var move_you = "rock";
-$("#play_you").text (move_you);
-console.log("you: " + move_you);
+/* Variables we need for the game */
+
+var choices = ["rock", "paper", "scissors"];
+
+var played = 0;
+var won = 0;
+var lost = 0;
+var draw = 0;
+
+/* Listen for the button clicks */
+$("button").click(function(event){
+var play_you = this.getAttribute("value");
+$("#play_you").text(play_you);
+var play_machine = computer_move();
+compare(play_you, play_machine);
 });
 
-$("#paper").click(function(event){
-var move_you = "paper";
-$("#play_you").text (move_you);
-console.log("you: " + move_you);
-});
+function computer_move() {
+  var play_machine = Math.floor(Math.random() * (3 - 0)) + 0;
 
-$("#Scissors").click(function(event){
-var move_you = "Scissors";
-$("#play_you").text (move_you);
-console.log("you: " + move_you);
-});
+  play_machine = choices[play_machine];
+  $("#play_machine").text(play_machine);
+  return play_machine;
+}
+
+function compare(you, machine){
+  var verdict = "lose";
+
+  if ( you == machine ) {
+    verdict = "draw";
+  } else if ((you == "rock") && (machine == "scissors")) {
+    verdict = "win";
+  } else if ((you == "paper") && (machine == "rock")) {
+    verdict = "win";
+  } else if  ((you == "scissors") && (machine == "paper")) {
+    verdict = "win";
+  }
+
+  switch (verdict) {
+    case "win":
+    won++;
+    $("#game_won").text(won);
+    break;
+  case "lose":
+    lost++;
+    $("#game_lost").text(lost);
+    break;
+  case "draw":
+    draw++;
+    $("#game_draw").text(draw);
+    break;
+  }
+  $("#verdict").text(verdict);
+
+  played++;
+  $("#game_played").text(played);
+}
